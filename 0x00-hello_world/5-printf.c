3d77f7938226d169280f8b5fc13df7fa9d2037f8
faezza <fatimaezzahrakhalid3@gmail.com>
2023-03-16 14:44:02 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this will print something
+ *
+ * Return: 0 when successfull
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
