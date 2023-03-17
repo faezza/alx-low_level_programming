da7c76f5319761ce3a8f3ede63da70064a92fda1
faezza <fatimaezzahrakhalid3@gmail.com>
2023-03-16 17:58:08 -0700

    hgjhuihiu

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - this function is printing a quote to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 59);
-    return(1);
+    write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
