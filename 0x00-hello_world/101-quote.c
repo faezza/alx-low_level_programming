fe4fad2bb9accec73245a8fb8c7aad1d94c9314d
faezza <fatimaezzahrakhalid3@gmail.com>
2023-03-16 18:20:33 -0700

    hjggg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - prints a quote to the standard error
+ * main - prints a quote
  *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
