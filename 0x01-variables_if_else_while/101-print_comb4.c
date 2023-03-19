e99edddd6c6bae55eafcdba9aff23ebf1e6815a6
faezza <fatimaezzahrakhalid3@gmail.com>
2023-03-19 13:15:44 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			for (k = 2 ; k < 10 ; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i + j + k != 24)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
