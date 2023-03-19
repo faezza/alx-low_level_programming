1b3b5ce0b38ca5e3815fb0fccfef25250d79876e
faezza <fatimaezzahrakhalid3@gmail.com>
2023-03-19 11:17:34 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
