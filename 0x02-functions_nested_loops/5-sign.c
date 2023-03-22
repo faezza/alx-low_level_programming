9de39419963a3e4546b083d8db0a466ec657609b
faezza <fatimaezzahrakhalid3@gmail.com>
2023-03-22 13:07:46 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,22 +13,32 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		return (0);
 	}
 	else
 	{
 		_putchar('-');
+	}
+	if (n != 0)
+	{
 		_putchar(',');
 		_putchar(' ');
+	}
+
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
 		return (-1);
 	}
+
 }
